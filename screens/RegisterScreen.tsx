50fb0375a260f6df45295b9cbe3a2a8554e422cc
Aline de Lima Santos <linsantos93@gmail.com>
2021-09-20 18:57:12 -0300

    Validações de formulario para tela de Registro

--- screens/RegisterScreen.tsx
+++ screens/RegisterScreen.tsx
@@ -1,8 +1,21 @@
+import { Formik, FormikErrors, FormikTouched } from "formik";
 import * as React from "react";
 import { StyleSheet, View, Image, TouchableOpacity, Text } from "react-native";
-import { Card, TextInput, Button, Paragraph } from "react-native-paper";
+import {
+  Card,
+  TextInput,
+  Button,
+  Paragraph,
+  HelperText,
+} from "react-native-paper";
 
 import { RootStackScreenProps } from "../types";
+import { registerValidationSchema } from "../utils/FormValidations";
+
+interface RegisterScreenProps {
+  field: { name: any; onBlur: any; onChange: any; value: any };
+  form: { errors: any; touched: any; setFieldTouched: any };
+}
 
 export default function RegisterScreen({
   navigation,
@@ -13,32 +26,114 @@ export default function RegisterScreen({
         style={styles.logo}
         source={require("../assets/images/logo.png")}
       />
-      <Card style={styles.card}>
-        <Card.Title title="Cadastre-se" />
-        <Card.Content>
-          <TextInput mode="outlined" label="Nome" />
-          <TextInput
-            textContentType="emailAddress"
-            mode="outlined"
-            label="E-mail"
-          />
-          <TextInput secureTextEntry mode="outlined" label="Senha" />
-          <TextInput secureTextEntry mode="outlined" label="Confirmar senha" />
-        </Card.Content>
-        <Card.Actions style={styles.actionsContainer}>
-          <Button mode="contained" color="#A7D86D" dark style={styles.button}>
-            Cadastrar
-          </Button>
-          <Paragraph>
-            Já possui uma conta?{" "}
-            <TouchableOpacity onPress={() => navigation.replace("Login")}>
-              <Text style={[styles.linkText, styles.registerLinkText]}>
-                Faça o login
-              </Text>
-            </TouchableOpacity>
-          </Paragraph>
-        </Card.Actions>
-      </Card>
+      <Formik
+        validationSchema={registerValidationSchema}
+        initialValues={{
+          name: "",
+          email: "",
+          password: "",
+          confirmPassword: "",
+        }}
+        onSubmit={(values) => console.log(values)}
+      >
+        {({
+          handleChange,
+          handleBlur,
+          handleSubmit,
+          values,
+          errors,
+          touched,
+          dirty,
+          isValid,
+        }) => (
+          <Card style={styles.card}>
+            <Card.Title title="Cadastre-se" />
+            <Card.Content>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  mode="outlined"
+                  label="Nome"
+                  value={values.name}
+                  onChangeText={handleChange("name")}
+                  onBlur={handleBlur("name")}
+                  error={errors.name !== undefined && touched.name}
+                />
+                {errors.name && touched.name && (
+                  <HelperText type="error"> {errors.name} </HelperText>
+                )}
+              </View>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  textContentType="emailAddress"
+                  mode="outlined"
+                  autoCompleteType="email"
+                  keyboardType="email-address"
+                  autoCapitalize="none"
+                  label="E-mail"
+                  value={values.email}
+                  onChangeText={handleChange("email")}
+                  onBlur={handleBlur("email")}
+                  error={errors.email !== undefined && touched.email}
+                />
+                {errors.email && touched.email && (
+                  <HelperText type="error"> {errors.email} </HelperText>
+                )}
+              </View>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  secureTextEntry
+                  mode="outlined"
+                  label="Senha"
+                  value={values.password}
+                  onChangeText={handleChange("password")}
+                  onBlur={handleBlur("password")}
+                  error={errors.password !== undefined && touched.password}
+                />
+                {errors.password && touched.password && (
+                  <HelperText type="error"> {errors.password} </HelperText>
+                )}
+              </View>
+              <View style={styles.textInputContainer}>
+                <TextInput
+                  secureTextEntry
+                  mode="outlined"
+                  label="Confirmar senha"
+                  value={values.confirmPassword}
+                  onChangeText={handleChange("confirmPassword")}
+                  onBlur={handleBlur("confirmPassword")}
+                  error={
+                    errors.confirmPassword !== undefined &&
+                    touched.confirmPassword
+                  }
+                />
+                {errors.confirmPassword && touched.confirmPassword && (
+                  <HelperText type="error">{errors.confirmPassword}</HelperText>
+                )}
+              </View>
+            </Card.Content>
+            <Card.Actions style={styles.actionsContainer}>
+              <Button
+                mode="contained"
+                color="#A7D86D"
+                dark
+                style={styles.button}
+                onPress={handleSubmit}
+                disabled={!dirty || !isValid}
+              >
+                Cadastrar
+              </Button>
+              <Paragraph>
+                Já possui uma conta?{" "}
+                <TouchableOpacity onPress={() => navigation.replace("Login")}>
+                  <Text style={[styles.linkText, styles.registerLinkText]}>
+                    Faça o login
+                  </Text>
+                </TouchableOpacity>
+              </Paragraph>
+            </Card.Actions>
+          </Card>
+        )}
+      </Formik>
     </View>
   );
 }
@@ -50,6 +145,9 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
+  },
+  textInputContainer: {
+    height: 85,
   },
   logo: {
     width: 204,
--- utils/FormValidations.ts
+++ utils/FormValidations.ts
@@ -7,3 +7,22 @@ export const loginValidationSchema = yup.object().shape({
     .required("Campo obrigatório"),
   password: yup.string().required("Campo obrigatório"),
 });
+
+export const registerValidationSchema = yup.object().shape({
+  name: yup
+    .string()
+    .matches(/(\w.+\s).+/, "Insira pelo menos 2 nomes")
+    .required("Campo obrigatório"),
+  email: yup
+    .string()
+    .email("Insira um e-mail válido")
+    .required("Campo obrigatório"),
+  password: yup
+    .string()
+    .min(6, ({ min }) => `Insira no mínimo ${min} caracteres`)
+    .required("Campo obrigatório"),
+  confirmPassword: yup
+    .string()
+    .oneOf([yup.ref("password")], "A senha digitada não confere")
+    .required("Campo obrigatório"),
+});
