cb4b7baedada14bffe5de08b4792c6178baec0c8
LucasTeixeiraDev <lucas.teixeira@grupoboticario.com.br>
2021-09-22 20:51:52 -0300

    Add Events datasource

---
+++ models/EventModel.ts
@@ -0,0 +1,9 @@
+export interface EventModel {
+  userId: string;
+  title: string;
+  address: string;
+  initialDate: string;
+  finishDate: string;
+  initialHour: number;
+  finishHour: number;
+}
